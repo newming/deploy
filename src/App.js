5c8888ca35945cfc3b7310e5e36087238a678c54
newming <a977527479@sina.com>
2016-08-02 15:37:57 +0800

    git search

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
+  getUserInfo(){
+    return axios.get(`https://api.github.com/users/newming`)
+      .then((xx) => (
+        { gitInfo: xx.data }
+    ))
+  }
+  constructor(){
+    super();
+    this.state={
+      info:{}
+    }
+  }
+  componentDidMount(){
+    this.getUserInfo().then((data) => {
+      console.log(data.gitInfo);
+      this.setState({
+        info:data.gitInfo
+      })
+    });
+  }
   render () {
     return(
-      <h1>脚手架工具</h1>
+      <div>
+        {this.state.info.blog},
+        {this.state.info.following},
+        {this.state.info.location},
+        <img src={this.state.info.avatar_url} />
+      </div>
     )
   }
 }
